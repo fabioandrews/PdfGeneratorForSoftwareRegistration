cfcd636dbac8affadba4a0f767e7d7bfba68c659
fábio andrews rocha marques <fabio_andrews@hotmail.com>
2015-10-19 14:57:55 -0200

    telaprincipal não é mais resizable

--- src/br/ufrn/pairg/interfacegrafica/TelaPrincipal.java
+++ src/br/ufrn/pairg/interfacegrafica/TelaPrincipal.java
@@ -191,7 +191,9 @@ public class TelaPrincipal extends JFrame implements ActionListener
 	/**
 	 * Create the frame.
 	 */
-	public TelaPrincipal() {
+	public TelaPrincipal() 
+	{
+		this.setResizable(false);
 		setTitle("code2inpi");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 690, 637);
