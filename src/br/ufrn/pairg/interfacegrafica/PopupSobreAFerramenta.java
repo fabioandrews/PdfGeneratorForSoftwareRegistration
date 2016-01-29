0acaf6c10f78e08a9dc94165e7d7f3b146831620
fabio phillip <fabiophillip@hotmail.com>
2016-01-29 14:02:00 -0300

    -about sem logo e url do dimap -icones do about levam pra URL

--- src/br/ufrn/pairg/interfacegrafica/PopupSobreAFerramenta.java
+++ src/br/ufrn/pairg/interfacegrafica/PopupSobreAFerramenta.java
@@ -1,7 +1,10 @@
 package br.ufrn.pairg.interfacegrafica;
 
 import java.awt.BorderLayout;
+import java.awt.Cursor;
+import java.awt.Desktop;
 import java.awt.FlowLayout;
+import java.awt.Rectangle;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
@@ -14,6 +17,8 @@ import java.awt.GridBagLayout;
 
 import javax.swing.JLabel;
 
+import com.sun.org.apache.xerces.internal.util.URI;
+
 import br.ufrn.pairg.pdfgenerator.CriaeLeArquivoConfiguracoesdat;
 import br.ufrn.pairg.pdfgenerator.Main;
 
@@ -21,6 +26,11 @@ import java.awt.GridBagConstraints;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 import java.net.URL;
 
 public class PopupSobreAFerramenta extends JDialog implements ActionListener {
@@ -35,7 +45,7 @@ public class PopupSobreAFerramenta extends JDialog implements ActionListener {
 			PopupSobreAFerramenta dialog = new PopupSobreAFerramenta();
 			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 			dialog.setVisible(true);
-			dialog.setTitle("Sobre Code2inpi");
+			dialog.setTitle("Sobre");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -55,9 +65,9 @@ public class PopupSobreAFerramenta extends JDialog implements ActionListener {
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		GridBagLayout gbl_contentPanel = new GridBagLayout();
-		gbl_contentPanel.columnWidths = new int[]{0, 0, 0, 0, 0, 0};
+		gbl_contentPanel.columnWidths = new int[]{0, 0};
 		gbl_contentPanel.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0};
-		gbl_contentPanel.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_contentPanel.columnWeights = new double[]{0.0, 0.0};
 		gbl_contentPanel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		contentPanel.setLayout(gbl_contentPanel);
 		{
@@ -66,10 +76,10 @@ public class PopupSobreAFerramenta extends JDialog implements ActionListener {
 			JLabel labelVersao = new JLabel("Vers\u00E3o: " + versao);
 			GridBagConstraints gbc_labelVersao = new GridBagConstraints();
 			gbc_labelVersao.insets = new Insets(0, 0, 5, 0);
-			gbc_labelVersao.gridx = 2;
+			gbc_labelVersao.gridx = 1;
 			gbc_labelVersao.gridy = 1;
 			gbc_labelVersao.gridheight = 1;
-			gbc_labelVersao.gridwidth = 1;
+			gbc_labelVersao.gridwidth = 2;
 			contentPanel.add(labelVersao, gbc_labelVersao);
 		}
 		{
@@ -77,23 +87,116 @@ public class PopupSobreAFerramenta extends JDialog implements ActionListener {
 			GridBagConstraints gbc_labelUrl = new GridBagConstraints();
 			gbc_labelUrl.insets = new Insets(0, 0, 5, 0);
 			gbc_labelUrl.gridx = 1;
-			gbc_labelUrl.gridy = 3;
+			gbc_labelUrl.gridy = 2;
 			gbc_labelUrl.gridheight = 1;
 			gbc_labelUrl.gridwidth = 2;
 			contentPanel.add(labelUrl, gbc_labelUrl);
+			labelUrl.setVisible(false);
 		}
 		{
-			URL urlImagemAbout = Main.class.getResource(
-                    "/resources/about.png");
-			Icon icon = new ImageIcon(urlImagemAbout);
-			JLabel labelImagemSponsors = new JLabel(icon);
+			URL urlImagemLogoUfrn = Main.class.getResource(
+                    "/resources/logoufrn.png");
+			Icon icon = new ImageIcon(urlImagemLogoUfrn);
+			final JLabel labelImagemLogoUFRN = new JLabel(icon);
+			GridBagConstraints gbc_labelImagemLogoUFRN = new GridBagConstraints();
+			gbc_labelImagemLogoUFRN.insets = new Insets(0, 0, 0, 5);
+			gbc_labelImagemLogoUFRN.gridx = 1;
+			gbc_labelImagemLogoUFRN.gridy = 4;
+			gbc_labelImagemLogoUFRN.gridheight = 2;
+			gbc_labelImagemLogoUFRN.gridwidth = 1;
+			contentPanel.add(labelImagemLogoUFRN, gbc_labelImagemLogoUFRN);
+			
+			
+			labelImagemLogoUFRN.addMouseListener(new MouseListener() {
+				
+				@Override
+				public void mouseReleased(MouseEvent e) {
+					// TODO Auto-generated method stub
+					
+				}
+				
+				@Override
+				public void mousePressed(MouseEvent e) {
+					// TODO Auto-generated method stub
+					
+				}
+				
+				@Override
+				public void mouseExited(MouseEvent e) {
+					e.getComponent().setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+					
+				}
+				
+				@Override
+				public void mouseEntered(MouseEvent e) {
+					e.getComponent().setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+					
+				}
+				
+				@Override
+				public void mouseClicked(MouseEvent e) 
+				{
+					try {
+						openWebpage(new URL("http://www.ufrn.br/"));
+					} catch (MalformedURLException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					
+				}
+			});
+			
+			
+			
+			URL urlImagemLogoPairg = Main.class.getResource(
+                    "/resources/logopairg.png");
+			Icon iconePairg = new ImageIcon(urlImagemLogoPairg);
+			JLabel labelImagemLogoPairg = new JLabel(iconePairg);
 			GridBagConstraints gbc_labelImagemSponsors = new GridBagConstraints();
 			gbc_labelImagemSponsors.insets = new Insets(0, 0, 0, 5);
-			gbc_labelImagemSponsors.gridx = 1;
+			gbc_labelImagemSponsors.gridx = 2;
 			gbc_labelImagemSponsors.gridy = 4;
 			gbc_labelImagemSponsors.gridheight = 2;
-			gbc_labelImagemSponsors.gridwidth = 2;
-			contentPanel.add(labelImagemSponsors, gbc_labelImagemSponsors);
+			gbc_labelImagemSponsors.gridwidth = 1;
+			contentPanel.add(labelImagemLogoPairg, gbc_labelImagemSponsors);
+			
+			labelImagemLogoPairg.addMouseListener(new MouseListener() {
+				
+				@Override
+				public void mouseReleased(MouseEvent e) {
+					// TODO Auto-generated method stub
+					
+				}
+				
+				@Override
+				public void mousePressed(MouseEvent e) {
+					// TODO Auto-generated method stub
+					
+				}
+				
+				@Override
+				public void mouseExited(MouseEvent e) {
+					e.getComponent().setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+					
+				}
+				
+				@Override
+				public void mouseEntered(MouseEvent e) {
+					e.getComponent().setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+					
+				}
+				
+				@Override
+				public void mouseClicked(MouseEvent e) {
+					try {
+						openWebpage(new URL("http://www.pairg.dimap.ufrn.br/"));
+					} catch (MalformedURLException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					
+				}
+			});
 		}
 		{
 			GridBagConstraints gbc_labelButtonPane = new GridBagConstraints();
@@ -115,6 +218,25 @@ public class PopupSobreAFerramenta extends JDialog implements ActionListener {
 	    {
 	    	this.dispose();
 	    }
-	} 
+	}
+	
+	public static void openWebpage(java.net.URI uri) {
+	    Desktop desktop = Desktop.isDesktopSupported() ? Desktop.getDesktop() : null;
+	    if (desktop != null && desktop.isSupported(Desktop.Action.BROWSE)) {
+	        try {
+	            desktop.browse(uri);
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        }
+	    }
+	}
+
+	public static void openWebpage(URL url) {
+	    try {
+	        openWebpage(url.toURI());
+	    } catch (URISyntaxException e) {
+	        e.printStackTrace();
+	    }
+	}
 
 }
